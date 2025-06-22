78a3986c9fedc79b87e1d0093e601ef6bb792adf
carloicorcuera <carloicorcuera@gmail.com>
2025-06-22 09:50:39 +0800

    production_v3.1.3

---
+++ app/modules/external/controller.js
@@ -0,0 +1,32 @@
+const { fetchInitialListings, fetchListingById } = require('./service');
+const AppError = require('../../utils/AppError');
+
+const getClientInitialListings = async (req, res, next) => {
+  try {
+    const listings = await fetchInitialListings();
+    res.status(200).json({
+      success: true,
+      timestamp: new Date().toISOString(),
+      data: listings,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+const getSingleListing = async (req, res, next) => {
+  const { ID } = req.params;
+
+  try {
+    const listing = await fetchListingById(ID);
+    res.status(200).json({
+      success: true,
+      data: listing,
+      timestamp: new Date().toISOString(),
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports = { getClientInitialListings, getSingleListing };
